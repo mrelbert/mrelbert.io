a9c3c711966dcfa9190a276443b87557ef32d9b0
Elbert Kim <elbert.kim@icloud.com>
2023-03-26 20:36:40 -0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { getBlogViews, getTweetCount, getStarCount } from 'lib/metrics';
 import {
   ArrowIcon,
-  GitHubIcon,
-  TwitterIcon,
-  ViewsIcon,
 } from 'components/icons';
 import { name, about, bio, avatar } from 'lib/info';
 
 export const revalidate = 60;
 
 export default async function HomePage() {
-  let starCount, views, tweetCount;
-
-  try {
-    [starCount, views, tweetCount] = await Promise.all([
-      getStarCount(),
-      getBlogViews(),
-      getTweetCount(),
-    ]);
-  } catch (error) {
-    console.error(error);
-  }
-
   return (
     <section>
       <h1 className="font-bold text-3xl font-serif">{name}</h1>
